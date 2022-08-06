2b9519296e28175727901cb083d9863982fa03fb
Eric Rodrigues <er601432@gmail.com>
2022-08-06 19:23:48 -0300

    init: init the lib

---
+++ src/index.js
@@ -0,0 +1,78 @@
+import FileReader from "filereader";
+import fs from "fs";
+const data = fs.readFileSync("data.csv", "utf-8");
+
+const fileReader = new FileReader();
+
+function replaceByIndex(str, index, replacement) {
+  return (
+    str.substr(0, index) + replacement + str.substr(index + replacement.length)
+  );
+}
+
+function csvFileToArrayImportData(data) {
+  const csvHeader = data.slice(0, data.indexOf("\n")).split(",");
+  const csvRows = data.slice(data.indexOf("\n") + 1).split("\n");
+
+  const csvRowsFormated = csvRows.map((row) => {
+    let newRow = row;
+
+    for (let i = 0; i < row.length; i++) {
+      if (row[i] === '"') {
+        for (let j = i + 1; j < row.length; j++) {
+          if (row[j] === '"') {
+            i = j;
+            break;
+          }
+
+          if (row[j] === ",") {
+            newRow = replaceByIndex(newRow, j, ";");
+          }
+        }
+      }
+    }
+
+    return newRow.replace(/"/g, "");
+  });
+
+  const array = csvRowsFormated.map((row) => {
+    const values = row.split(",");
+
+    const formtedValue = csvHeader.reduce((object, header, index) => {
+      if (String(values[index]).match(/;/)) {
+        object[header] = String(values[index]).split(";");
+      } else {
+        object[header] = values[index];
+      }
+
+      return object;
+    }, {});
+
+    return formtedValue;
+  });
+
+  array.splice(array.length - 1, 1);
+
+  return array;
+}
+
+function importCsv(csv) {
+  if (csv) {
+    fileReader.onload = function (event) {
+      const text = event.target.result;
+      csvFileToArrayImportData(text);
+    };
+
+    fileReader.readAsText(csv);
+  }
+}
+
+// (async () => {
+//   console.log("OPA");
+//   const test = csvFileToArrayImportData(data);
+
+//   console.log(test);
+//   //await importCsv(data);
+// })();
+
+export { csvFileToArrayImportData, importCsv };
